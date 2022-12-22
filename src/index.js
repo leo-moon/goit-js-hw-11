d872f974f7e001c320bee0528a9055ba2f3e8524
Vladimir Larin <lvv0820@gmail.com>
2022-12-22 03:06:06 +0200

    3.05

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { createCards } from './js/create-cards'
 
 let simpleLightBox;
 let searchQuery = '';
+const perPage = 4;
 
 const refs = {
   searchForm : document.querySelector('.search-form'),
@@ -27,7 +28,7 @@ function onSubmit(e) {
   // if (searchQuery === '') { return }
   let currentPage = 1;
 
-  fetchImages(searchQuery, currentPage).then(({ data }) => {
+  fetchImages(searchQuery, currentPage, perPage).then(({ data }) => {
     const images = data;
     console.log(images);
     console.log('aaa', data.totalHits, data.hits)
@@ -52,13 +53,15 @@ function onSubmit(e) {
     currentPage += 1
     simpleLightBox.destroy()
 
-    fetchImages(searchQuery, currentPage)
+    fetchImages(searchQuery, currentPage, perPage)
       .then(({ data }) => {
         createCards(data.hits)
         simpleLightBox = new SimpleLightbox('.gallery a').refresh()
 
-        const aa = -2
-        if (!(aa)) {console.log(aa)}
+        if  (data.totalHits < perPage * currentPage) {    
+          refs.loadMoreBtn.classList.add('is-hidden');
+          console.log('End')
+        }
 
 
         // const totalPages = Math.ceil(data.totalHits / perPage)
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,16 +1,8 @@
 import axios from 'axios';
 
-export async function fetchImages(searchQuery, page) {
-  // export async function fetchImages(page,searchQuery ) {
+export async function fetchImages(searchQuery, page, perPage) {
   const BASE_URL = 'https://pixabay.com/api/?key=32187725-9ebb8484d7ffd0cb9d2ef83f1';
-  const pp = 4;
-  // searchQuery = 'cat';
-  // console.log(pp);
+  const urlAdress = BASE_URL + `&q=${searchQuery}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  const urlAdress = BASE_URL + `&q=${searchQuery}&page=${page}&per_page=${pp}&image_type=photo&orientation=horizontal&safesearch=true`;
-  // const urlAdress = BASE_URL + `&q='cat'&page=${page}&per_page=${pp}&image_type=photo&orientation=horizontal`;
-
-  // console.log(urlAdress);
   return await axios.get(urlAdress)
 };
-// export default fetchImages 
